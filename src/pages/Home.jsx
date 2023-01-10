0972e718274d4654bcca4998d86341fc93d88b13
Romain Rey <romainrey.contact@gmail.com>
2023-01-10 18:54:19 +0100

    feat: :sparkles: Update page Home with Banner

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,24 @@
 import React from "react";
+import Image from "../images/img-home.jpg";
+import Header from "../components/Header/Header";
+import Banner from "../components/Banner/Banner";
+import ListOfThumbs from "../components/ListOfThumbs/ListOfThumbs";
+import Footer from "../components/Footer/Footer";
 
 export default function Home() {
-
     return (
         <>
-            <h1>Test</h1>
+            <Header />
+            <main>
+                <Banner>
+                    <img src={Image} alt="Une montagne embrumée" />
+                    <h1>
+                        Chez vous, partout et ailleurs
+                    </h1>
+                </Banner>
+                <ListOfThumbs />
+            </main>
+            <Footer />
         </>
     )
 }
