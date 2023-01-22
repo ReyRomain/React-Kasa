7a227c4e6920ad07bb9616bd8a42ab027c4f1e22
Romain Rey <romainrey.contact@gmail.com>
2023-01-22 18:36:16 +0100

    feat: :art: Add scss for Banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 export default function Banner({children}) {
     return (
-        <>
-            <section>
+        <React.Fragment>
+            <section className="banner">
                 {children}
             </section>
-        </>
+        </React.Fragment>
     )
 }
