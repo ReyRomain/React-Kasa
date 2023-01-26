cc3d8bf984dbb93b820649d2380eae39df9c7939
Romain Rey <romainrey.contact@gmail.com>
2023-01-26 13:26:25 +0100

    feat: :rocket: Add route Error

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
 import Accommodation from './pages/Accommodation';
 import About from './pages/About';
+import Error from './pages/Error';
 import './App.css';
 
 export default function App() {
     return (
 		<BrowserRouter>
 			<Routes>
-				<Route path='/' element={<Home />} />
-                <Route path='/accommodation/:id' element={<Accommodation />} />
-				<Route path='/about' element={<About />} />
+				<Route exact path='/' element={<Home />} />
+                <Route exact path='/accommodation/:id' element={<Accommodation />} />
+				<Route excat path='/about' element={<About />} />
+				<Route path='*' element={<Error />} />
 			</Routes>
 		</BrowserRouter>
     );
